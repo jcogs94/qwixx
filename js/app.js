6c503ee2f095d7ab74ae038b0cfdd8772b955f5d
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-11 11:17:02 -0400

    gameState object, readies the game to begin

--- js/app.js
+++ js/app.js
@@ -3,4 +3,13 @@
 import * as elements from "./elements.js";
 import * as gameFunctions from "./gameFunctions.js";
 
-gameFunctions.clearBoard();
+// Object to reference game state
+let gameState = {
+    start: true
+}
+
+// Initializes the start of a new game
+if (gameState.start === true) {
+    gameFunctions.clearBoard();
+    gameState.start = false;
+}
