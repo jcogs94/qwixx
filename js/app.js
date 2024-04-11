5e0d147ba4433ffe8e24ad15d353563a99c3ca3d
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-11 10:04:40 -0400

    element definition moved to separate file

--- js/app.js
+++ js/app.js
@@ -1,42 +1,5 @@
 // Obtained from following link:
 // https://www.scaler.com/topics/javascript/import-js-file-in-js/
-// import { getElement } from "./elements";
+import { redElements, yellowElements, greenElements, blueElements } from "./elements.js";
 
-// Define empty objects to obtain button elements by color
-let redElements = {};
-let yellowElements = {};
-let greenElements = {};
-let blueElements = {};
-
-// Obtain red button elements
-const redRowElement = document.querySelector('.red');
-const redElementChildren = redRowElement.children;
-for (let element of redElementChildren) {
-    redElements[element.innerHTML] = element;
-}
-
-// Obtain yellow button elements
-const yellowRowElement = document.querySelector('.yellow');
-const yellowElementChildren = yellowRowElement.children;
-for (let element of yellowElementChildren) {
-    yellowElements[element.innerHTML] = element;
-}
-
-// Obtain green button elements
-const greenRowElement = document.querySelector('.green');
-const greenElementChildren = greenRowElement.children;
-for (let element of greenElementChildren) {
-    greenElements[element.innerHTML] = element;
-}
-
-// Obtain blue button elements
-const blueRowElement = document.querySelector('.blue');
-const blueElementChildren = blueRowElement.children;
-for (let element of blueElementChildren) {
-    blueElements[element.innerHTML] = element;
-}
-
-console.log(redElements);
-console.log(yellowElements);
-console.log(greenElements);
-console.log(blueElements);
+console.log(redElements.L);
--- js/elements.js
+++ js/elements.js
@@ -1,6 +1,36 @@
-const getElement = () => {
-    const ${name}Element = document.querySelector(selector);
-    ${name}Element.
+// Define empty objects to obtain button elements by color
+let redElements = {};
+let yellowElements = {};
+let greenElements = {};
+let blueElements = {};
+
+// Obtain red button elements
+const redRowElement = document.querySelector('.red');
+const redElementChildren = redRowElement.children;
+for (let element of redElementChildren) {
+    redElements[element.innerHTML] = element;
 }
 
-export { getElement };
+// Obtain yellow button elements
+const yellowRowElement = document.querySelector('.yellow');
+const yellowElementChildren = yellowRowElement.children;
+for (let element of yellowElementChildren) {
+    yellowElements[element.innerHTML] = element;
+}
+
+// Obtain green button elements
+const greenRowElement = document.querySelector('.green');
+const greenElementChildren = greenRowElement.children;
+for (let element of greenElementChildren) {
+    greenElements[element.innerHTML] = element;
+}
+
+// Obtain blue button elements
+const blueRowElement = document.querySelector('.blue');
+const blueElementChildren = blueRowElement.children;
+for (let element of blueElementChildren) {
+    blueElements[element.innerHTML] = element;
+}
+
+// Exports all objects
+export { redElements, yellowElements, greenElements, blueElements };
