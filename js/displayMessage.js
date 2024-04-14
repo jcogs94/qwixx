d824b45ed0761ff5365e307c3bee9d7b385679ce
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-14 15:57:35 -0400

    options, displays and removes appropriately throughout game

--- js/displayMessage.js
+++ js/displayMessage.js
@@ -11,10 +11,73 @@ const blueOptionsElement = document.querySelector('#blue-options');
 const options = () => {
     // Updates options with current values
     whiteOptionsElement.innerHTML = `${gameState.rollValues.whiteTotal}`;
-    redOptionsElement.innerHTML = `${gameState.rollValues.redWhite1} / ${gameState.rollValues.redWhite2}`;
-    yellowOptionsElement.innerHTML = `${gameState.rollValues.yellowWhite1} / ${gameState.rollValues.yellowWhite2}`;
-    greenOptionsElement.innerHTML = `${gameState.rollValues.greenWhite1} / ${gameState.rollValues.greenWhite2}`;
-    blueOptionsElement.innerHTML = `${gameState.rollValues.blueWhite1} / ${gameState.rollValues.blueWhite2}`;
+
+    // If statements below allow the options per color to only display one option if the combination
+    // with both white dice are the same. Also removes that color from being displayed if it is no
+    // longer in play
+    if (gameState.colorInPlay.red) {
+        if (document.querySelector('#red-options') === null) {
+            whiteOptionsElement.insertAdjacentElement('afterend', redOptionsElement);
+        }
+        
+        if (gameState.rollValues.redWhite1 === gameState.rollValues.redWhite2) {
+            redOptionsElement.innerHTML = `${gameState.rollValues.redWhite1}`;
+        } else {
+            redOptionsElement.innerHTML = `${gameState.rollValues.redWhite1} / ${gameState.rollValues.redWhite2}`;
+        }
+    } else {
+        if (document.querySelector('#red-options') !== null) {
+            redOptionsElement.remove();
+        }
+    }
+
+    if (gameState.colorInPlay.yellow) {
+        if (document.querySelector('#yellow-options') === null) {
+            redOptionsElement.insertAdjacentElement('afterend', yellowOptionsElement);
+        }
+
+        if (gameState.rollValues.yellowWhite1 === gameState.rollValues.yellowWhite2) {
+            yellowOptionsElement.innerHTML = `${gameState.rollValues.yellowWhite1}`;
+        } else {
+            yellowOptionsElement.innerHTML = `${gameState.rollValues.yellowWhite1} / ${gameState.rollValues.yellowWhite2}`;
+        }
+    } else {
+        if (document.querySelector('#yellow-options') !== null) {
+            yellowOptionsElement.remove();
+        }
+    }
+
+    if (gameState.colorInPlay.green) {
+        if (document.querySelector('#green-options') === null) {
+            yellowOptionsElement.insertAdjacentElement('afterend', greenOptionsElement);
+        }
+
+        if (gameState.rollValues.greenWhite1 === gameState.rollValues.greenWhite2) {
+            greenOptionsElement.innerHTML = `${gameState.rollValues.greenWhite1}`;
+        } else {
+            greenOptionsElement.innerHTML = `${gameState.rollValues.greenWhite1} / ${gameState.rollValues.greenWhite2}`;
+        }
+    } else {
+        if (document.querySelector('#green-options') !== null) {
+            greenOptionsElement.remove();
+        }
+    }
+
+    if (gameState.colorInPlay.blue) {
+        if (document.querySelector('#blue-options') === null) {
+            greenOptionsElement.insertAdjacentElement('afterend', blueOptionsElement);
+        }
+
+        if (gameState.rollValues.blueWhite1 === gameState.rollValues.blueWhite2) {
+            blueOptionsElement.innerHTML = `${gameState.rollValues.blueWhite1}`;
+        } else {
+            blueOptionsElement.innerHTML = `${gameState.rollValues.blueWhite1} / ${gameState.rollValues.blueWhite2}`;
+        }
+    } else {
+        if (document.querySelector('#blue-options') !== null) {
+            blueOptionsElement.remove();
+        }
+    }
     
     // Insert heading and options elements to dom if previously removed
     if (document.querySelector('#options-heading-box') === null) {
